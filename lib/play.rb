bbedb0fd0d216292686bd506253fb32326eb7d90
Ekaterina Zarudnaya <zar.catherine@gmail.com>
2020-03-28 02:19:53 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,7 +37,7 @@ end
 
 # Define your play method below
 def play(board)
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
   while turn < 9
     turn += 1
   end
