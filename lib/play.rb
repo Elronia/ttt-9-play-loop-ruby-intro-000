86e603a0d0a49dea56429c72511bb845e8bf1c04
Ekaterina Zarudnaya <zar.catherine@gmail.com>
2020-03-28 02:14:48 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,5 +37,10 @@ end
 
 # Define your play method below
 def play(board)
-
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  while turn < 9
+    turn += 1
+  end
+    turn(board)
+    play(board)
 end
